d95af37f0b8cfee73c2b6953d02568e9650f05c7
Krishna Kaushik <46679308+krishnakaushik25@users.noreply.github.com>
2021-09-12 13:47:27 +0530

    Update mal.js

--- WebApp/static/js/mal.js
+++ WebApp/static/js/mal.js
@@ -5,7 +5,7 @@ var map = null;
 
     var val = $("#id_one").val();
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', "http://127.0.0.1:8000", true);
+    xhr.open('POST', "http://34.125.17.109:8000", true);
     data = new FormData();
     data.append("type", val);
     if(val != "cancer")
